d633ae46042d8907a5ef029397271ab86bff78a3
Novilawati <secretk242@gmail.com>
2020-02-10 11:13:31 +0800

    Nambah Login Activity

---
+++ BelajarXML1/app/src/main/java/com/example/belajar_xml_1/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.belajar_xml_1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
